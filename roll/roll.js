4e9d0494bd6747fca3e18dfdbc1e41e3be6b1c26
Mikael Dahlqvist <zeractor@gmail.com>
2024-02-21 08:26:01 +0100

    NEW DIRECTION

--- roll/roll.js
+++ roll/roll.js
@@ -1,82 +1,58 @@
-const splitDice = require('./splitDice');
-
 let trackedIds = {};
 let isGM = false;
 let me;
 
+window.addEventListener("load", (event) => {
+});
+
 function roll(type) {
-    let nameId = '';
     let diceId = '';
     switch (type)
     {
         case "standard":
-            nameId = 'roll-name-standard-attack';
             diceId = 'roll-content-standard-attack';
         case "full":
-            nameId = 'roll-name-full-attack';
             diceId = 'roll-content-full-attack';
     }
-    if (nameId == '' || diceId == '')
+    if (diceId == '')
         return;
 
-    const name = document.getElementById(nameId).value;
-    const dice = document.getElementById(diceId).value;
-    const allDice = splitDice(name, dice);
+    const name = type;
+    let dice = document.getElementById(diceId).value;
+    let allDice = [];
+    for(let i = 0; i < dice; i++) {
+        allDice.push({name: 'attack' + (i + 1), roll: 'd20'});
+    }
     TS.dice.putDiceInTray(allDice, true).then((diceSetResponse) => {
         trackedIds[diceSetResponse] = type;
     });
 }
 
 async function handleRollResult(rollEvent) {
+    let rollId = rollEvent.payload.rollId;
     if (trackedIds[rollEvent.payload.rollId] == undefined) {
         //if we haven't tracked that roll, ignore it because it's not from us
+        console.log('untracked roll');
         return;
     }
 
     if (rollEvent.kind == "rollResults") {
+        console.log('tracked roll');
         //user rolled the dice we tracked and there's a new result for us to look at
-        let roll = rollEvent.payload
-        let finalResult = 0;
-        let resultGroup = {};
-        if (roll.resultsGroups != undefined && roll.resultsGroups.length >= 2) {
-            //just making sure the roll actually has 2 or more groups. should never be false as we created the roll with 2 groups
-            if (trackedIds[roll.rollId] == "advantage") {
-                //the incoming roll was stored as an advantage roll
-                let max = 0;
-                for (let group of roll.resultsGroups) {
-                    let groupSum = await TS.dice.evaluateDiceResultsGroup(group);
-                    //if you want to check if the result returned here has an error, checking for groupSum.cause != undefined works
-                    if (groupSum > max) {
-                        max = groupSum;
-                        resultGroup = group;
-                    }
-                }
-                finalResult = max;
-            } else if (trackedIds[roll.rollId] == "disadvantage") {
-                //the incoming roll was stored as an disadvantage roll
-                let min = Number.MAX_SAFE_INTEGER;
-                for (let group of roll.resultsGroups) {
-                    let groupSum = await TS.dice.evaluateDiceResultsGroup(group);
-                    //if you want to check if the result returned here has an error, checking for groupSum.cause != undefined works
-                    if (groupSum < min) {
-                        min = groupSum;
-                        resultGroup = group;
-                    }
-                }
-                finalResult = min == Number.MAX_SAFE_INTEGER ? 0 : min;
-            } else {
-                return;
-            }
+        let roll = rollEvent.payload;
+        let critTrigger = document.getElementById('min-crit-range').value;
+        let threatDice = [];
+
+        for(let group of roll.resultsGroups) {
+            let groupSum = await TS.dice.evaluateDiceResultsGroup(group);
+            if (groupSum >= critTrigger)
+                threatDice.push({name:'threat', roll:'d20'});
         }
 
-        //finalResult remains unused in this example, but could be useful for other applications
-        displayResult(resultGroup, roll.rollId);
+        TS.dice.putDiceInTray(threatDice, false);
+        TS.dice.sendDiceResult(roll.resultsGroups, rollId).catch((response) => console.error("error in sending dice result", response));
     } else if (rollEvent.kind == "rollRemoved") {
         //if you want special handling when the user doesn't roll the dice
         delete trackedIds[rollEvent.payload.rollId];
     }
-}
-
-async function displayResult(resultGroup, rollId) {
-    TS.dice.sendDiceResult([resultGroup], rollId).catch((response) => console.error("error in sending dice result", response));
 }
--- roll/splitDice.js
+++
@@ -1,9 +0,0 @@
-function splitDice(name, diceOptions) {
-    const allDice = [];
-    diceOptions.split('+').forEach((d, i) => {
-        allDice.push({ name: name + '(' + i + ')', roll: d });
-    });
-    return allDice;
-}
-
-module.exports = splitDice;
